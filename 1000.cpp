1f9c7b767b2466b760c5e500e04cd5cbd7205bb4
4solo <34310696@qq.com>
2015-08-18 19:03:12 -0700

    problem 1000

---
+++ 1000.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  刷题
+//
+//  Created by sky on 15/8/18.
+//  Copyright (c) 2015年 sky. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a, b;
+   while(cin>>a>>b)
+    {
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
